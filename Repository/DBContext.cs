2756c721a096c22fe7e2cce4c5f7b0fc20515ce6
Marcin Patela <marcin.patela@windowslive.com>
2025-04-02 09:20:46 +0200

    Changed: added base for EF Changed: gitignore is now proper for w .net solution

---
+++ Repository/DBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DBContext
+    {
+
+        public class MBankContext : DbContext
+        {
+
+            public MBankContext()
+            {
+
+            }
+            public MBankContext(DbContextOptions<MBankContext> options) : base(options)
+            {
+
+            }
+            //public DbSet<Company> Companies { get; set; }
+            //public DbSet<Invoice> Invoices { get; set; }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@" Server=80.211.254.208; Database=ISZPDev;Trusted_Connection=false;user=Jacek.Patela;Password=1q2w3e4r;TrustServerCertificate=True;");
+
+
+        }
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Repository.DBContext;
+
+namespace Repository
+{
+    public class RepositoryBase
+    {
+        public MBankContext db = new MBankContext(new DbContextOptionsBuilder<MBankContext>()
+                .UseSqlServer(@" Server=80.211.254.208; Database=ISZPDev;Trusted_Connection=false;user=Jacek.Patela;Password=1q2w3e4r;TrustServerCertificate=True;")
+                .Options);
+
+    }
+}
